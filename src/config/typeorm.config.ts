ef71b82108898f0ca1922bd8b3e5857c60ba9056
Nde Lucien <38033535+dark-matter08@users.noreply.github.com>
2023-07-27 09:11:58 +0100

    generated auth moduels

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,33 +2,31 @@ import {
   TypeOrmModuleAsyncOptions,
   TypeOrmModuleOptions,
 } from '@nestjs/typeorm';
-import { ConfigService, ConfigModule } from '@nestjs/config';
-
-export default class TypeOrmConfig {
-  static getOrmConfig(configService: ConfigService): TypeOrmModuleOptions {
-    return {
-      type: 'postgres',
-      host: configService.get('DB_HOST'),
-      port: configService.get('DB_PORT'),
-      username: configService.get('DB_USERNAME'),
-      password: configService.get('DB_PASSWORD'),
-      database: configService.get('DB_NAME'),
-      entities: [__dirname + '/../**/*.entity.{ts,js}'],
-      synchronize: true,
-      logging: true,
-    };
-  }
-}
 
 export const typeOrmConfigAsync: TypeOrmModuleAsyncOptions = {
-  imports: [ConfigModule],
-  inject: [ConfigService],
-  useFactory: async (
-    configService: ConfigService,
-  ): Promise<TypeOrmModuleOptions> => TypeOrmConfig.getOrmConfig(configService),
+  useFactory: async () => {
+    return {
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
+      username: process.env.DB_USERNAME,
+      database: process.env.DB_NAME,
+      password: process.env.DB_PASSWORD,
+      entities: [__dirname + '/../**/*.entity.{js,ts}'],
+      logging: true,
+      synchronize: false,
+      migrations: [__dirname + '/../database/migrations/*{.ts,.js}'],
+      extra: {
+        charset: 'utf8mb4_unicode_ci',
+      },
+      cli: {
+        migrationsDir: __dirname + '/../database/migrations',
+      },
+    };
+  },
 };
 
-export const typeOrmConfig: TypeOrmModuleOptions = {
+export const typeOrmConfig = {
   type: 'postgres',
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT, 10),
@@ -36,9 +34,52 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  migrations: [__dirname + '/../database/migrations/*{.ts,.js}'],
   extra: {
     charset: 'utf8mb4_unicode_ci',
   },
   synchronize: false,
   logging: true,
+  cli: {
+    migrationsDir: __dirname + '/../database/migrations',
+  },
 };
+
+// export default class TypeOrmConfig {
+//   static getOrmConfig(configService: ConfigService): TypeOrmModuleOptions {
+//     return {
+//       type: 'postgres',
+//       host: configService.get('DB_HOST'),
+//       port: configService.get('DB_PORT'),
+//       username: configService.get('DB_USERNAME'),
+//       password: configService.get('DB_PASSWORD'),
+//       database: configService.get('DB_NAME'),
+//       entities: [__dirname + '/../**/*.entity.{ts,js}'],
+//       synchronize: true,
+//       logging: true,
+//     };
+//   }
+// }
+
+// export const typeOrmConfigAsync: TypeOrmModuleAsyncOptions = {
+//   imports: [ConfigModule],
+//   inject: [ConfigService],
+//   useFactory: async (
+//     configService: ConfigService,
+//   ): Promise<TypeOrmModuleOptions> => TypeOrmConfig.getOrmConfig(configService),
+// };
+
+// export const typeOrmConfig: TypeOrmModuleOptions = {
+//   type: 'postgres',
+//   host: process.env.DB_HOST,
+//   port: parseInt(process.env.DB_PORT, 10),
+//   username: process.env.DB_USERNAME,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   entities: [__dirname + '/../**/*.entity.{js,ts}'],
+//   extra: {
+//     charset: 'utf8mb4_unicode_ci',
+//   },
+//   synchronize: false,
+//   logging: true,
+// };
